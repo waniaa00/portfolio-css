d933c5030820fefab618ca943e0f395466048ad6
wania akram <160944972+waniaa00@users.noreply.github.com>
2024-12-31 16:56:55 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="hidden lg:block"></div>
         <div className="hero-title">
           <div className="hero-main">
-            <p data-aos="zoom-in-up">I'm</p>
+            <p data-aos="zoom-in-up">I&apos;m</p>
             <p data-aos="zoom-in-up">Wania</p>
             <p data-aos="zoom-in-up">Akram</p>
           </div>
