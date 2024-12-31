dbde8080bb9c01bdda4c5874910733a53b6f9e0b
wania akram <160944972+waniaa00@users.noreply.github.com>
2024-12-31 16:55:28 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,11 +6,9 @@ const About = () => {
     <div id='about' className='about-container' >
         <h2 className='about-heading' data-aos="zoom-in-up" >About me</h2>
         <p className='about-text' data-aos="zoom-in-up" >
-            I'm a student at GIAIC, cultivating proficiencies in Artificial Intelligence, Web 3.0 & Metaverse.
+            I&apos;m a student at GIAIC, cultivating proficiencies in Artificial Intelligence, Web 3.0 & Metaverse.
             Infatuated by technological advancements and perpetually immersing myself in new skills to stay consistent with emerging trends.
-
         </p>
-      
     </div>
   )
 }
